ebc7ac31a2e329375abd0a863bb48533f81569a1
Giov4nnaxx <gigisantanasilva@gmail.com>
2025-10-31 09:00:34 -0300

    Primeiro commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+// const prevBtn = document.getElementById("prev-btn");
+// const nextBtn = document.getElementById("next-btn");
+// const carousselContent = document.getElementById("caroussel-content");
+
+
+// const images = [
+//     'public/images/ft biblioteca.jpg',
+//     'public/images/ft livros.jpg',
+//     'public/imagesft biblioteca2.jpg',
+//     'public/images/ft livros2.jpg',
+//     'public/images/ft biblioteca3.jpg',
+//     'public/images/ft livros3.jpg'
+// ];
+
+// let currentIndex = 0;
+
+// function updateCaroussel() {
+//     const imgPath = images[currentIndex];
+
+//     carousselContent.style.backgroundImage = `url("${imgPath}")`;
+// }
+
+// nextBtn.addEventListener("click", () => {
+//     currentIndex = (currentIndex + 1) % images.length;
+//     updateCaroussel();
+// });
+
+// prevBtn.addEventListener("click", () => {
+//     currentIndex = (currentIndex - 1 + images.length) % images.length;
+//     updateCaroussel();
+// });
+
+
+// updateCaroussel();
+
+console.log("Vapo")
